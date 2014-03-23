36d70b3d86d4fbf8859149f8208c9d318fd5744a
James <jamieoliverband@gmail.com>
2014-03-23 21:49:44 +0100

    jhj

--- assignment1/src/main/java/tuwien/ir/assignment1/App.java
+++ assignment1/src/main/java/tuwien/ir/assignment1/App.java
@@ -1,3 +1,35 @@
+<<<<<<< HEAD
+package tuwien.ir.assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// The CLI allows the user to enter provide
+    	// search parameters and a file with a topic to search for.
+    	// bag-of-words or the bi-gram index, scoring method)
+    	
+    	if (args.length == 0){
+    		printHelp();
+    	}else{
+    		// TODO run through the parameters and choose the appropriate action
+    	}
+    	
+        System.out.println( "Hello World!" );
+    }
+
+	private static void printHelp() {
+		System.out.println("Help\n----------");
+		System.out.print("Your input should be looking like this: ");
+		System.out.println("assignment1 -i bow -f file.txt\n");
+		System.out.println("-i(index): \tbow ...bag of words index\n\t\tbig ...bigram-index\n");
+		System.out.println("-sm(scoring method): \t TODO ");
+	}
+}
 package tuwien.ir.assignment1;
 
 /**
@@ -33,3 +65,75 @@ public class App
 		System.out.println("-sm(scoring method): \t TODO ");
 	}
 }
+package tuwien.ir.assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// The CLI allows the user to enter provide
+    	// search parameters and a file with a topic to search for.
+    	// bag-of-words or the bi-gram index, scoring method)
+    	
+    	if (args.length == 0){
+    		printHelp();
+    	}else{
+    		// TODO run through the parameters and choose the appropriate action
+    		for(int i = 0; i < args.length ;i++){
+    			String s = args[i];
+    			
+    		}
+    	}
+    	
+        System.out.println( "Hello World!" );
+    }
+
+	private static void printHelp() {
+		System.out.println("Help\n----------");
+		System.out.print("Your input should be looking like this: ");
+		System.out.println("assignment1 -i bow -f file.txt\n");
+		System.out.println("-i(index): \tbow ...bag of words index\n\t\tbig ...bigram-index\n");
+		System.out.println("-sm(scoring method): \t TODO ");
+	}
+}
+=======
+package tuwien.ir.assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// The CLI allows the user to enter provide
+    	// search parameters and a file with a topic to search for.
+    	// bag-of-words or the bi-gram index, scoring method)
+    	
+    	if (args.length == 0){
+    		printHelp();
+    	}else{
+    		// TODO run through the parameters and choose the appropriate action
+    		for(int i = 0; i < args.length ;i++){
+    			String s = args[i];
+    			
+    		}
+    	}
+    	
+        System.out.println( "Hello World!" );
+    }
+
+	private static void printHelp() {
+		System.out.println("Help\n----------");
+		System.out.print("Your input should be looking like this: ");
+		System.out.println("assignment1 -i bow -f file.txt\n");
+		System.out.println("-i(index): \tbow ...bag of words index\n\t\tbig ...bigram-index\n");
+		System.out.println("-sm(scoring method): \t TODO ");
+	}
+}
+>>>>>>> ADD VocabularyImplTest; § does not get deleted.
