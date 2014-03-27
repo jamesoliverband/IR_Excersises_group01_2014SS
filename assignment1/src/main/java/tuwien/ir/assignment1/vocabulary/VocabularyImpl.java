6c020369161f4e6dc72c75c1208c86d55c7ecfd3
jamieoliverband@gmail.com <jamieoliverband@gmail.com>
2014-03-27 13:18:28 +0100

    ADD Stoplist

--- assignment1/src/main/java/tuwien/ir/assignment1/vocabulary/VocabularyImpl.java
+++ assignment1/src/main/java/tuwien/ir/assignment1/vocabulary/VocabularyImpl.java
@@ -3,15 +3,56 @@
  */
 package tuwien.ir.assignment1.vocabulary;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author OliverS
- *
+ * 
  */
 public class VocabularyImpl implements Vocabulary {
 
+	private List<String> stopwords;
+
+	public VocabularyImpl() {
+		stopwords = new ArrayList<String>();
+
+		try {
+			// Open the file that is the first
+			// command line parameter
+			String current = new java.io.File( "." ).getCanonicalPath();
+	        System.out.println("Current dir:"+current);
+			FileInputStream fstream = new FileInputStream("./target/classes/stoplist.txt");
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			// Read File Line By Line
+			while ((strLine = br.readLine()) != null) {
+				// Print the content on the console
+				stopwords.add(strLine);
+				System.out.println(strLine);
+			}
+			// Close the input stream
+			in.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+
 	/*
 	 * (non-Javadoc)
-	 * @see tuwien.ir.assignment1.vocabulary.Vocabulary#tokenize(java.lang.String)
+	 * 
+	 * @see
+	 * tuwien.ir.assignment1.vocabulary.Vocabulary#tokenize(java.lang.String)
 	 */
 	public String[] tokenize(String s) {
 		String[] words = s.split(" ");
@@ -20,41 +61,49 @@ public class VocabularyImpl implements Vocabulary {
 
 	/*
 	 * (non-Javadoc)
-	 * @see tuwien.ir.assignment1.vocabulary.Vocabulary#caseFolding(java.lang.String[])
+	 * 
+	 * @see
+	 * tuwien.ir.assignment1.vocabulary.Vocabulary#caseFolding(java.lang.String
+	 * [])
 	 */
 	public String[] caseFolding(String[] s) {
 
-		for(int i = 0; i < s.length; i++){
+		for (int i = 0; i < s.length; i++) {
 			s[i] = s[i].toLowerCase().replaceAll("\\p{Punct}", "");
 		}
-		
+
 		return s;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see tuwien.ir.assignment1.vocabulary.Vocabulary#removingStopWords(java.lang.String[], java.lang.String[])
+	 * 
+	 * @see
+	 * tuwien.ir.assignment1.vocabulary.Vocabulary#removingStopWords(java.lang
+	 * .String[], java.lang.String[])
 	 */
-	public String[] removingStopWords(String[] s, String[] stopWords) {		
-		for(int i = 0; i < s.length; i++){
-			for(int r = 0; r < stopWords.length;r++){
-				if(s[i] == stopWords[i]){
+	public String[] removingStopWords(String[] s, String[] stopWords) {
+		for (int i = 0; i < s.length; i++) {
+			for (int r = 0; r < stopWords.length; r++) {
+				if (s[i] == stopWords[i]) {
 					// TODO delete from list
 					s[i] = "";
 				}
 			}
 		}
-		
+
 		return s;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see tuwien.ir.assignment1.vocabulary.Vocabulary#stemming(java.lang.String)
+	 * 
+	 * @see
+	 * tuwien.ir.assignment1.vocabulary.Vocabulary#stemming(java.lang.String)
 	 */
 	public String stemming(String s) {
 		Stemming stemming = new Stemming();
-				
+
 		return stemming.stemming(s);
 	}
 
