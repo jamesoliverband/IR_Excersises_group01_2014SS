e0192bdde8d4d3dc978d70b2699d312c3362b48d
Ernst Rohlicek <ernst@rohlicek.at>
2014-03-31 14:57:48 +0200

    ADD stubs for Bag-of-words and Bi-gram index

---
+++ assignment1/src/main/java/tuwien/ir/assignment1/index/BigramIndex.java
@@ -0,0 +1,24 @@
+package tuwien.ir.assignment1.index;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class BigramIndex implements Index {
+
+	public void indexDocument(File document, String topicId, String documentId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ArrayList<SearchResult> getSimilarDocuments(File document,
+			String runName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void load(File indexFile) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment1/src/main/java/tuwien/ir/assignment1/index/BowIndex.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package tuwien.ir.assignment1.index;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * @author Administrator
+ *
+ */
+public class BowIndex implements Index {
+
+	public void indexDocument(File document, String topicId, String documentId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ArrayList<SearchResult> getSimilarDocuments(File document, String runName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void load(File indexFile) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
