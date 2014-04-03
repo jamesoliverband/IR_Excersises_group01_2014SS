d9409d3adf03163f5827ae701e9d9a3f1bd0a384
Ernst Rohlicek <ernst@rohlicek.at>
2014-04-03 23:16:10 +0200

    HitsOverallSearch: implementation done HitsSeldomSearch: implementation done

--- assignment1/src/main/java/tuwien/ir/assignment1/search/HitsOverallSearch.java
+++ assignment1/src/main/java/tuwien/ir/assignment1/search/HitsOverallSearch.java
@@ -3,8 +3,15 @@ package tuwien.ir.assignment1.search;
 public class HitsOverallSearch implements Search {
 
 	public double search(String[] document, String[] topic) {
-		// TODO Auto-generated method stub
-		return 0;
+		int hits = 0;
+		for (int i=0; i < topic.length; i++) {
+			for (int j=0; j < document.length; j++) {
+				if (topic[i].equals(document[j])) {
+					hits += 1;
+				}
+			}
+		}
+		return hits / document.length;
 	}
 
 }
--- assignment1/src/main/java/tuwien/ir/assignment1/search/HitsSeldomSearch.java
+++ assignment1/src/main/java/tuwien/ir/assignment1/search/HitsSeldomSearch.java
@@ -1,10 +1,45 @@
 package tuwien.ir.assignment1.search;
 
+import java.util.HashMap;
+
 public class HitsSeldomSearch implements Search {
 
 	public double search(String[] document, String[] topic) {
 		// TODO Auto-generated method stub
-		return 0;
+		HashMap<String, Integer> counts = new HashMap<String, Integer>();
+		// count each word
+		for (int i=0; i < document.length; i++) {
+			// get word
+			String word = document[i];
+			// increase count
+			if (counts.containsKey(word)) {
+				counts.put(word, counts.get(word) + 1);
+			} else {
+				counts.put(word,  1);
+			}
+		}
+		// get seldom word threshold (50 % of max value)
+		Integer[] vals = (Integer[]) counts.values().toArray();
+		Integer maxCount = 0;
+		for (int i=0; i < vals.length; i++) {
+			// get count value
+			Integer val = vals[i];
+			// decide
+			if (val > maxCount) {
+				maxCount = val;
+			}
+		}
+		maxCount = maxCount / 2;	// max count value in order to qualify as seldom word
+		// regular hits overall algorithm, but only over the seldom words
+		int hits = 0;
+		for (int i=0; i < topic.length; i++) {
+			for (int j=0; j < document.length; j++) {
+				if (topic[i].equals(document[j]) && counts.get(document[j]) <= maxCount) {
+					hits += 1;
+				}
+			}
+		}
+		return hits / document.length;
 	}
 
 }
